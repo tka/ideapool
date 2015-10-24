dec09f4ac3e5b0fe2f86dda40dbf22f681425d6f
tka lu <tka.lu@commandp.com>
2015-10-25 02:22:59 +0800

    WIP: deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'idea_pool'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ideapool'
+set :repo_url, 'git@github.com:tka/ideapool.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/ideapool'
 
 # Default value for :scm is :git
 # set :scm, :git
