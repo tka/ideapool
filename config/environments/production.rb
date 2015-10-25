7ff2416981ab5ed1194eb811655ef4018e9d6485
tka.lu <tkatka@gmail.com>
2015-10-26 00:07:38 +0800

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings= Setting.smtp
+  config.action_mailer.smtp_settings= Settings.smtp
 end
